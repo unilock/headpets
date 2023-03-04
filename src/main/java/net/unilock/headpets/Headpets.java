9058ff5031a3a4d24ce46fe43973c73e695f52cf
unilock <unilock@fennet.rentals>
2023-03-03 19:37:56 -0500

    init
    Signed-off-by: unilock <unilock@fennet.rentals>

---
+++ src/main/java/net/unilock/headpets/Headpets.java
@@ -0,0 +1,13 @@
+package net.unilock.headpets;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Headpets implements ModInitializer {
+    /**
+     * Runs the mod initializer.
+     */
+    @Override
+    public void onInitialize() {
+
+    }
+}
