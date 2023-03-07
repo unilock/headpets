d7b01642564afd4a9c334988d2f316ac1b659262
unilock <unilock@fennet.rentals>
2023-03-07 08:44:21 -0500

    the mod!!
    Signed-off-by: unilock <unilock@fennet.rentals>

---
+++ README.md
@@ -0,0 +1,11 @@
+# headpets
+
+sneak + right-click a player to pet them <3
+
+***
+
+TODO:
+
+- play a sound when petting someone
+- allow sounds to be configured per player (via file and/or in-game command(?))
+- allow mod to be installed client-side, which just adds the hand-swinging animation when you pet someone (i tried to implement this server-side and just could not figure it out umu )
---
+++ src/main/java/net/unilock/headpets/mixin/PlayerEntityMixin.java
@@ -0,0 +1,33 @@
+package net.unilock.headpets.mixin;
+
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.particle.ParticleTypes;
+import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.Hand;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(PlayerEntity.class)
+public class PlayerEntityMixin {
+    @Inject(method = "interact", at = @At("HEAD"), cancellable = true)
+    public void headPetMoment(Entity entity, Hand hand, CallbackInfoReturnable<ActionResult> cir) {
+        var interactor = (PlayerEntity) (Object) this;
+
+        if(interactor instanceof ServerPlayerEntity petter && !petter.getWorld().isClient()) {
+            if (!petter.isSpectator() && petter.isSneaking() && petter.getActiveHand().equals(Hand.MAIN_HAND) && petter.getStackInHand(Hand.MAIN_HAND).isEmpty()) {
+                // separate conditional for when i feel like adding support for more entities
+                if (entity instanceof ServerPlayerEntity pettee) {
+                    var petteeEyePos = pettee.getEyePos();
+
+                    petter.getWorld().spawnParticles(ParticleTypes.HEART, petteeEyePos.x, petteeEyePos.y + 0.5, petteeEyePos.z, 1, 0, 0, 0, 0.1);
+
+                    cir.setReturnValue(ActionResult.SUCCESS);
+                }
+            }
+        }
+    }
+}
