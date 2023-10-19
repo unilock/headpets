0c8a3237a60f1b0d6f4a08c9c419306c25dd3cce
unilock <unilock@fennet.rentals>
2023-10-19 18:47:12 -0400

    minor mixin improvements
    Signed-off-by: unilock <unilock@fennet.rentals>

--- src/main/java/cc/unilock/headpets/mixin/PlayerEntityMixin.java
+++ src/main/java/cc/unilock/headpets/mixin/PlayerEntityMixin.java
@@ -7,24 +7,25 @@ import net.minecraft.server.world.ServerWorld;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.Hand;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(PlayerEntity.class)
 public class PlayerEntityMixin {
+	@Unique
+	private final PlayerEntity petter = (PlayerEntity) (Object) this;
+
     @Inject(method = "interact", at = @At("HEAD"), cancellable = true)
     public void headPetMoment(Entity entity, Hand hand, CallbackInfoReturnable<ActionResult> cir) {
-        var petter = (PlayerEntity) (Object) this;
-
         if (!petter.isSpectator() && petter.isSneaking() && petter.getStackInHand(hand).isEmpty()) {
             // TODO: separate conditional for when i feel like adding support for more entities
-            if (entity instanceof PlayerEntity pettee) {
-                if (petter.getWorld() instanceof ServerWorld) {
-                    var petteeEyePos = pettee.getEyePos();
-                    var petteeWorld = (ServerWorld) pettee.getWorld();
+            if (entity instanceof PlayerEntity) {
+                if (!petter.getWorld().isClient()) {
+                    var petteeEyePos = entity.getEyePos();
 
-                    petteeWorld.spawnParticles(ParticleTypes.HEART, petteeEyePos.x, petteeEyePos.y + 0.5, petteeEyePos.z, 1, 0, 0, 0, 0.1);
+					((ServerWorld) entity.getWorld()).spawnParticles(ParticleTypes.HEART, petteeEyePos.getX(), petteeEyePos.getY() + 0.5, petteeEyePos.getZ(), 1, 0, 0, 0, 0.1);
                 } else {
                     petter.swingHand(hand);
                 }
