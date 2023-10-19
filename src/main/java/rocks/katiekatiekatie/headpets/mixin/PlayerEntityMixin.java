56ea267254d60871b5b6f913a33c1745f38e6339
unilock <unilock@fennet.rentals>
2023-10-19 18:30:35 -0400

    1.19.4 and stuff
    Signed-off-by: unilock <unilock@fennet.rentals>

--- src/main/java/rocks/katiekatiekatie/headpets/mixin/PlayerEntityMixin.java
+++ src/main/java/rocks/katiekatiekatie/headpets/mixin/PlayerEntityMixin.java
@@ -3,7 +3,7 @@ package rocks.katiekatiekatie.headpets.mixin;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.particle.ParticleTypes;
-import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.server.world.ServerWorld;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.Hand;
 import org.spongepowered.asm.mixin.Mixin;
@@ -15,18 +15,21 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 public class PlayerEntityMixin {
     @Inject(method = "interact", at = @At("HEAD"), cancellable = true)
     public void headPetMoment(Entity entity, Hand hand, CallbackInfoReturnable<ActionResult> cir) {
-        var interactor = (PlayerEntity) (Object) this;
+        var petter = (PlayerEntity) (Object) this;
 
-        if(interactor instanceof ServerPlayerEntity petter && !petter.getWorld().isClient()) {
-            if (!petter.isSpectator() && petter.isSneaking() && petter.getActiveHand().equals(Hand.MAIN_HAND) && petter.getStackInHand(Hand.MAIN_HAND).isEmpty()) {
-                // separate conditional for when i feel like adding support for more entities
-                if (entity instanceof ServerPlayerEntity pettee) {
+        if (!petter.isSpectator() && petter.isSneaking() && petter.getStackInHand(hand).isEmpty()) {
+            // TODO: separate conditional for when i feel like adding support for more entities
+            if (entity instanceof PlayerEntity pettee) {
+                if (petter.getWorld() instanceof ServerWorld) {
                     var petteeEyePos = pettee.getEyePos();
+                    var petteeWorld = (ServerWorld) pettee.getWorld();
 
-                    petter.getWorld().spawnParticles(ParticleTypes.HEART, petteeEyePos.x, petteeEyePos.y + 0.5, petteeEyePos.z, 1, 0, 0, 0, 0.1);
-
-                    cir.setReturnValue(ActionResult.SUCCESS);
+                    petteeWorld.spawnParticles(ParticleTypes.HEART, petteeEyePos.x, petteeEyePos.y + 0.5, petteeEyePos.z, 1, 0, 0, 0, 0.1);
+                } else {
+                    petter.swingHand(hand);
                 }
+
+                cir.setReturnValue(ActionResult.SUCCESS);
             }
         }
     }
